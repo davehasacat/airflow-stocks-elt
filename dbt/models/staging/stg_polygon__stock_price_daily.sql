bfedf562e5a14e7df96abbc6b63bf3f09bd4e027
Dave Anaya <danaya2846@gmail.com>
2025-09-18 22:12:48 -0500

    update dbt file naming conventions

---
+++ dbt/models/staging/stg_polygon__stock_price_daily.sql
@@ -0,0 +1,22 @@
+with source as (
+
+    select * from {{ source('stocks_polygon', 'stg_polygon__stock_price_daily') }}
+
+),
+
+renamed_and_casted as (
+
+    select
+        cast(date as date) as date,
+        ticker,
+        cast(open_price as numeric(18, 4)) as open_price,
+        cast(high_price as numeric(18, 4)) as high_price,
+        cast(low_price as numeric(18, 4)) as low_price,
+        cast(close_price as numeric(18, 4)) as close_price,
+        cast(volume as integer) as volume
+
+    from source
+
+)
+
+select * from renamed_and_casted
