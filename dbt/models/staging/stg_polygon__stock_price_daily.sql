21a7697d2388eeb7dacad05c4d813d3dd3318efc
Dave Anaya <danaya2846@gmail.com>
2025-09-18 22:14:59 -0500

    clean up syntax in stg sql file

--- dbt/models/staging/stg_polygon__stock_price_daily.sql
+++ dbt/models/staging/stg_polygon__stock_price_daily.sql
@@ -5,18 +5,15 @@ with source as (
 ),
 
 renamed_and_casted as (
-
-    select
-        cast(date as date) as date,
-        ticker,
-        cast(open_price as numeric(18, 4)) as open_price,
-        cast(high_price as numeric(18, 4)) as high_price,
-        cast(low_price as numeric(18, 4)) as low_price,
-        cast(close_price as numeric(18, 4)) as close_price,
-        cast(volume as integer) as volume
-
-    from source
-
+select
+ cast(date as date) as date,
+ ticker,
+ cast(open_price as numeric(18, 4)) as open_price,
+ cast(high_price as numeric(18, 4)) as high_price,
+ cast(low_price as numeric(18, 4)) as low_price,
+ cast(close_price as numeric(18, 4)) as close_price,
+ cast(volume as integer) as volume
+from source
 )
 
 select * from renamed_and_casted
