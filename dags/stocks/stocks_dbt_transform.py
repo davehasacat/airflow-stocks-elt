2fac5670695b4363533e155351a3ac3c1da7f268
Dave Anaya <danaya2846@gmail.com>
2025-09-19 18:21:22 -0500

    update dbt transform dag with defualt args

--- dags/stocks/stocks_dbt_transform.py
+++ dags/stocks/stocks_dbt_transform.py
@@ -2,12 +2,20 @@ import pendulum
 from airflow.decorators import dag
 from cosmos import DbtTaskGroup, ProjectConfig, ProfileConfig
 
+# Best Practice: Define default arguments for all tasks in the DAG
+default_args = {
+    "owner": "airflow",
+    "retries": 2,
+    "retry_delay": pendulum.duration(minutes=1)
+}
+
 @dag(
     dag_id="stocks_dbt_transform",
     start_date=pendulum.datetime(2025, 1, 1, tz="UTC"),
     schedule=None, # This DAG will be triggered by another process
     catchup=False,
     tags=["dbt", "transformation"],
+    default_args=default_args # Apply the default args to the DAG
 )
 def stocks_dbt_transform_dag():
     """
@@ -26,6 +34,9 @@ def stocks_dbt_transform_dag():
         operator_args={
             "install_deps": True, # Ensures dbt dependencies are installed
         },
+        # Best Practice: You can also apply default_args directly to the TaskGroup
+        # if you want different settings than the rest of the DAG.
+        # default_args=default_args 
     )
 
 stocks_dbt_transform_dag()
